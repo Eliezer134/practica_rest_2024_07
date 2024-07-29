537bb06a77b2b2b2461c46e78df8e149ae4a5099
Nixon Gómez <eliezer134@gmail.com>
2024-07-28 20:51:59 -0500

    Renombrado de clase Controller

--- src/main/java/com/proteccion/rest/controller/HelloWorldController.java
+++ src/main/java/com/proteccion/rest/controller/HelloWorldController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class HolaMundoController {
+public class HelloWorldController {
 
   private final String HELLO_SERVICE = "/service";
   private final String HELLO_REPOSITORY = "/repository";
